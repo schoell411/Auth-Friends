5cc309140949bdd3201d270d0c0e6648ddec4755
Kevin Schoell <schoell411@gmail.com>
2020-01-28 22:01:03 -0600

    added route and link to login page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,6 @@
 //dependencies
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 //components
 import Login from "./components/Login";
@@ -9,10 +10,17 @@ import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Friends App</h1>
-			<Login />
-		</div>
+		<Router>
+			<div className="App">
+				<h1>Friends List App</h1>
+				<div>
+					<Link to="/login">Login</Link>
+				</div>
+				<Switch>
+					<Route path="/login" component={Login} />
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
