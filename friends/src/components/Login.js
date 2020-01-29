d3871da811624afcad77add6c026c6d144aac57c
Kevin Schoell <schoell411@gmail.com>
2020-01-29 02:21:57 -0600

    added change handler to the inputs

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+	state = {
+		credentials: {
+			username: "",
+			password: ""
+		}
+	};
+
+	handleChange = e => {
+		this.setState({
+			credentials: {
+				...this.state.credentials,
+				[e.target.name]: e.target.value
+			}
+		});
+	};
 	render() {
 		return (
 			<div>
 				<form>
-					<input type="text" name="username" />
-					<input type="password" name="password" />
+					<input
+						type="text"
+						name="username"
+						value={this.state.credentials.username}
+						onChange={this.handleChange}
+					/>
+					<input
+						type="password"
+						name="password"
+						value={this.state.credentials.password}
+						onChange={this.handleChange}
+					/>
 					<button>Log In</button>
 				</form>
 			</div>
